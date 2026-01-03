1f63c63da7c029ea22975be4dd0b91af0eacc1bc
yhyaa79 <162740371+yhyaa79@users.noreply.github.com>
2026-01-03 15:46:21 +0330

    update

---
+++ static/js/script.js
@@ -0,0 +1,25 @@
+
+// اضافه کردن حالت active به قسمت tab-btn 
+document.querySelectorAll('.tab-btn').forEach(button => {
+    button.addEventListener('click', () => {
+        // اول کلاس active را از همه دکمه‌ها بردار
+        document.querySelectorAll('.tab-btn').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // سپس به دکمه کلیک‌شده اضافه کن
+        button.classList.add('active');
+    });
+});
+
+
+// اضافه کردن حالت active به قسمت file-name 
+document.querySelectorAll('.file-name').forEach(button => {
+    button.addEventListener('click', () => {
+        // اول کلاس active را از همه دکمه‌ها بردار
+        document.querySelectorAll('.file-name').forEach(btn => {
+            btn.classList.remove('active');
+        });
+        // سپس به دکمه کلیک‌شده اضافه کن
+        button.classList.add('active');
+    });
+});
